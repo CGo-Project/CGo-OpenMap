d1797050c73da9950788a8c411cca55d46b81b2e
James NaL <liu0zihan@live.cn>
2026-09-15 11:58:55 +0800

    更新OpenMap图标

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * ==============================================================================
  */
 
-const CACHE_NAME = 'cgo-openmap-v260915.0014';
+const CACHE_NAME = 'cgo-openmap-v260915.0015';
 const ASSETS_TO_CACHE = [
     // 页面与入口
     './',
