7b3bcd88e48c6342b95b0c1da30c9dd42f41e896
Zhengping Si <sizhengping@hotmail.com>
2026-09-19 20:59:59 +0800

    chore: 暂时隐藏首页在线编辑器入口

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * ==============================================================================
  */
 
-const CACHE_NAME = 'cgo-openmap-v260920.0200';
+const CACHE_NAME = 'cgo-openmap-v260920.0201';
 const ASSETS_TO_CACHE = [
     // 页面与入口
     './',
