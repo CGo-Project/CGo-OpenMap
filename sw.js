3b8bb96958fe73b3834d227d00cb40516cd00528
James NaL <liu0zihan@live.cn>
2026-09-22 23:41:11 +0800

    更新CGoUI内容

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * ==============================================================================
  */
 
-const CACHE_NAME = 'cgo-openmap-v260922.1220';
+const CACHE_NAME = 'cgo-openmap-v260922.2340';
 const ASSETS_TO_CACHE = [
     // 页面与入口
     './',
