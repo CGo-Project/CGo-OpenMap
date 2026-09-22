a1efd665900f885df32ca7c0c540f2a375b6f239
James NaL <liu0zihan@live.cn>
2026-09-22 12:17:50 +0800

    更新CGoUI图标

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * ==============================================================================
  */
 
-const CACHE_NAME = 'cgo-openmap-v260922.0025';
+const CACHE_NAME = 'cgo-openmap-v260922.1220';
 const ASSETS_TO_CACHE = [
     // 页面与入口
     './',
