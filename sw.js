7f1377c6e7cb9afc9c7228e7ef688976bda2c9d8
James NaL <liu0zihan@live.cn>
2026-09-24 02:16:52 +0800

    chore(cgoui): 同步 CGoUI 2.1.0 最新产物并刷新 PWA 缓存版本

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  * ==============================================================================
  */
 
-const CACHE_NAME = 'cgo-openmap-v260923.1217';
+const CACHE_NAME = 'cgo-openmap-v260924.0216';
 const ASSETS_TO_CACHE = [
     // 页面与入口
     './',
